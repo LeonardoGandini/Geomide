b6f5240f62bbc36cc7a81b66654c388af4d0ebfe
Leonardo <leonardo@kaitendesign.com>
2020-05-17 11:16:37 +0100

    andando avanti

--- new-phlog.sh
+++ new-phlog.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-# __   __               
-# \ \ / / __ _  _ _  ___
-#  \ V / / _` || '_|(_-<
-#   \_/  \__,_||_|  /__/
-#                       
+
 phdir="/home/neko/Geomide/gopher-hole/phlog/"
-gtoday="$(date +'%Y-%m-%d_%H,%M')"
+gtoday="$(date +'%Y-%m-%d--%H-%M')"
 nl=$'\n'
 
 #Ask for post title
@@ -14,23 +10,19 @@ read -p  'Post Title: ' gophtitle
 #Ask for post file name
 read -p  'File Title: ' gophfiletitle
 
-#Post title created with figles
+#Post title created with figlet
 fancytitle=$(figlet -t -k -f /usr/share/figlet/mini.flf $gophtitle)
 
-phlogpost=$phdir/$gtoday$gophfiletitle.txt
+phlogpost=$phdir/$gtoday_$gophfiletitle.txt
 
 cat <<EOT >> $phlogpost
 $fancytitle
-ホリネズミHorinezumi $gtoday 
+ホリネズミ Horinezumi $gtoday 
 ☆::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::☆"
 $nl
 EOT
+
+#Open post with vim at the bottom
 vi +20 $phlogpost
-#
-#echo $gophtitle | figlet -t -k -f /usr/share/figlet/mini.flf 
-#echo "ホリネズミHorinezumi" $gtoday 
-#echo "☆::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::☆"
-#echo ""
-#echo ""
 
 
